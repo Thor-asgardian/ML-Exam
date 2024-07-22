7b3dcd9aac4e194a07e554c488b5726a0c2757f9
Bhuvan.R <144478466+Thor-asgardian@users.noreply.github.com>
2024-07-22 17:36:33 +0530

    Update KNN.py

--- Question-006/KNN.py
+++ Question-006/KNN.py
@@ -3,37 +3,43 @@ import pandas as pd
 from collections import Counter
 from sklearn.model_selection import train_test_split
 
-
+# Euclidian distance: ((x1 - x2)^2 + (y1 - y2)^2)^(1/2)
 def euclidean_distance(x1, x2):
-  return np.sqrt(np.sum((x1 - x2) ** 2))
+    return np.sqrt(np.sum((x1 - x2) ** 2))
 
+# Euclidian distance: |(x1 - x2) + (y1 - y2)|
 def manhattan_distance(x1, x2):
- return np.sum(np.abs(x1 - x2))
+    return np.sum(np.abs(x1 - x2))
 
 class KNN:
-  def __init__(self, k, distance_fn):
-    self.k = k
-    self.distance_fn = distance_fn
+    def __init__(self, k, distance_fn):
+        self.k = k
+        self.distance_fn = distance_fn
 
-  def fit(self, X, y):
-    self.X_train = X
-    self.y_train = y
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
 
-  def predict(self, X):
-    predictions = [self._predict(x) for x in X]
-    return predictions
+    def predict(self, X):
+        predictions = [self._predict(x) for x in X]
+        return predictions
 
-  def _predict(self, x):
-    distances = [self.distance_fn(x, x_train) for x_train in self.X_train]
-    k_indices = np.argsort(distances)[:self.k]
-    k_nearest_labels = [self.y_train[i] for i in k_indices]
-    most_common = Counter(k_nearest_labels).most_common()
-    return most_common[0][0]
+    def _predict(self, x):
+        distances = [self.distance_fn(x, x_train) for x_train in self.X_train]
+        k_indices = np.argsort(distances)[:self.k]
+        k_nearest_labels = [self.y_train[i] for i in k_indices]
+        most_common = Counter(k_nearest_labels).most_common(1)
+        return most_common[0][0]
 
+# read the data
 df = pd.read_csv('glass.csv')
+
+# Split the values
 y = df['Type'].values
 X = df.drop('Type', axis=1).values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+# Train and test the data
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # KNN with Euclidean distance
 clf_euclidean = KNN(k=3, distance_fn=euclidean_distance)
